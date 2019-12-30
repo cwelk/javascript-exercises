5cf10673aa02c26befaea5368a0d680802876307
Christian Welk <c.welk@getsafe.de>
2019-12-30 14:27:38 +0100

    Update description

--- exercises/Stack/index.js
+++ exercises/Stack/index.js
@@ -1,17 +1,22 @@
 /**
  * Stack
  *
+ * Part I:
  * Create a stack data structure. The stack
  * should be a class with methods 'push', 'pop', and
  * 'peek'.  Adding an element to the stack should
  * store it until it is removed.
  *
- * Additional description => pls add specs for this on your own!
+ * Do not use the native Array implementations of pop and push.
+ *
+ * Part II:
  *
  * The stack should never throw overflow errors and
  * should give a way to check the size of the stack.
  * Additionally the stack should be able to have an
  * upper limit.
+ *
+ * Please add specs for this on your own!
  *
  * Examples:
  * const s = new Stack();
